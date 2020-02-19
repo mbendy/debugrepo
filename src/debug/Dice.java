36b7768c7c2008b20b768f24a8fc3f6ecdd4c6e9
miabendy <miabendy@mias-mbp.dhcp.wustl.edu>
2020-02-19 10:56:41 -0600

    cleaned

--- src/debug/Dice.java
+++ src/debug/Dice.java
@@ -1,39 +1,63 @@
 package debug;
 
 public class Dice {
+	
+	public static int[] rollDie(int numDie) {
+		int [] dice = new int[numDie];
+		
+		for (int i = 0; i < numDie; i++) {
+			dice[i] = (int)(Math.random() * 7);
+			System.out.print(dice[i] + " ");
+
+		}
+		return dice;
+	}
+	
+	public static int sumRoll(int[] dice) {
+		int sum = 0; 
+		for(int i = 0; i < dice.length; i++) {
+			sum += dice[i];
+		}
+		return sum; 
+	}
+	
+	public static boolean checkYahtzee(int [] dice) {
+		boolean yahtzee = true; 
+		for(int j = 1; j < dice.length; j++) {
+            if(dice[0] != dice[j]) {
+                yahtzee = false; 
+                break; 
+            }
+        }
+		return yahtzee; 
+	}
 
     public static void main(String[] args) {
-        int dice = 4;
-        int rolls = 7;
+        int numDice = 4;
+        int numRolls = 7;
 
-        int[][] allDice = new int[rolls][dice];
-        int[] frequency = new int[dice * 6  - dice];
+        int[][] allDice = new int[numRolls][numDice];
+        int[] frequency = new int[numDice * 6  + 1];
 
-        int sum = 0;
         int yahtzee = 0;
-        for(int i = 0; i < dice; i++) {
+        for(int i = 0; i < numRolls; i++) {
             System.out.print("Roll: ");
-            for(int j = 0; j < rolls; j++) {
-                int die = (int)(Math.random() * 7);
-                allDice[i][j] = die;
-                sum += die;
-                System.out.print(die + " ");
-            }
+            allDice[i] = rollDie(numDice);
+            int sum = sumRoll(allDice[i]);
             System.out.println(" Sum: " + sum);
-
-            for(int j = 0; j < dice; j++) {
-                if(allDice[i][j] == allDice[i][j+1]) {
-                    yahtzee++;
-                }
-            }
             frequency[sum]++;
+            if (checkYahtzee(allDice[i])) {
+            	yahtzee++; 
+            }
         }
+        
         System.out.println("There were " + yahtzee + " yahtzees");
 
         for(int i = 0; i < frequency.length; i++) {
-            System.out.println("Sum: " + i + " times: " + frequency[i]);
+        	if (frequency[i] != 0) {
+                System.out.println("Sum: " + i + " times: " + frequency[i]);
+        	}
         }
-
     }
 
 }
